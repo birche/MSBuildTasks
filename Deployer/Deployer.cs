0d6b881cff1221e49515d8de7577fb39d059a7d0
Joachim Birche <joachim@birche.com>
2017-05-19 00:12:23 +0200

    New props, still testing

--- Deployer/Deployer.cs
+++ Deployer/Deployer.cs
@@ -6,14 +6,21 @@ namespace DeployerTarget
 {
     public class Deployer : Microsoft.Build.Utilities.Task
     {
+        public string PublishedOutput { get; set; }
+        public string PackageName { get; set; }
         public string ApplicationName { get; set; }
+        public string ApplicationVersion { get; set; }
+        public string Rid { get; set; }
 
         public override bool Execute()
         {
             //Debugger.Launch();
             Log.LogMessage(MessageImportance.High, "Hello from Deployer!");
-
-            Console.WriteLine("Hello world, from Deployer console...");
+            Log.LogMessage(MessageImportance.High, $"PublishedOutput=${PublishedOutput}");
+            Log.LogMessage(MessageImportance.High, $"PackageName={PackageName}");
+            Log.LogMessage(MessageImportance.High, $"ApplicationName={ApplicationName}");
+            Log.LogMessage(MessageImportance.High, $"ApplicationVersion={ApplicationVersion}");
+            Log.LogMessage(MessageImportance.High, $"Rid={Rid}");
 
             return true;
         }
