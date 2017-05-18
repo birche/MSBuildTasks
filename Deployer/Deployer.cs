e24bf8e8a9a83beca6af510807aacbc892686280
Joachim Birche <joachim.birche@beijerelectronics.com>
2017-05-18 17:55:37 +0200

    First commit

---
+++ Deployer/Deployer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Build.Framework;
+
+namespace DeployerTarget
+{
+    public class Deployer : Microsoft.Build.Utilities.Task
+    {
+        public string ApplicationName { get; set; }
+
+        public override bool Execute()
+        {
+            //Debugger.Launch();
+            Log.LogMessage(MessageImportance.High, "Hello from Deployer!");
+
+            Console.WriteLine("Hello world, from Deployer console...");
+
+            return true;
+        }
+    }
+}
