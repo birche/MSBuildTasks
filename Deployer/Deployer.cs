b652f7e70fed27ffc28f022b656e2f46d8d43654
Joachim Birche <joachim@birche.com>
2017-05-21 13:50:50 +0200

    Fixed zipper

--- Deployer/Deployer.cs
+++ Deployer/Deployer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
 using Microsoft.Build.Framework;
+using System.IO;
+using System.IO.Compression;
 
 namespace DeployerTarget
 {
@@ -12,6 +14,34 @@ namespace DeployerTarget
         public string ApplicationVersion { get; set; }
         public string Rid { get; set; }
 
+        private string ZipPublishedOutput()
+        {
+            var pubout = Path.Combine(Directory.GetCurrentDirectory(), PublishedOutput);
+            var filename = Path.Combine(pubout, $"{ApplicationName}-{ApplicationVersion}.zip");
+            Log.LogMessage(MessageImportance.High, $"Filename: {filename}");
+            Log.LogMessage(MessageImportance.High, $"PubOut: {pubout}");
+            if(File.Exists(filename))
+                File.Delete(filename);
+            
+            var tempFile = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            Log.LogMessage(MessageImportance.High, $"TempFile: {tempFile}");
+            try {
+                ZipFile.CreateFromDirectory(pubout, tempFile);
+                Log.LogMessage(MessageImportance.High, "Did zip the output");
+                File.Copy(tempFile, filename);
+                Log.LogMessage(MessageImportance.High, $"Did copy to {filename}");
+                return filename;
+            } 
+            catch 
+            {
+                throw;
+            } 
+            finally 
+            {
+                File.Delete(tempFile);
+            }
+        }
+
         public override bool Execute()
         {
             //Debugger.Launch();
@@ -22,6 +52,16 @@ namespace DeployerTarget
             Log.LogMessage(MessageImportance.High, $"ApplicationVersion={ApplicationVersion}");
             Log.LogMessage(MessageImportance.High, $"Rid={Rid}");
 
+            try 
+            {
+                var zipFile = ZipPublishedOutput();
+                Log.LogMessage(MessageImportance.High, $"Published output is now packed to the file {zipFile}");
+            }
+            catch
+            {
+                Log.LogError("Failed to zip the published output");
+                return false;
+            }
             return true;
         }
     }
